d023d19b378f12f678ab394b71bd421f9c72ea3d
Misha Krul <mkrul@spoonflower.com>
2018-12-21 09:11:22 -0500

    Update reloader.py

--- reloader.py
+++ reloader.py
@@ -1,4 +1,5 @@
-# Reloads the "browser" when it detects changes to the byte size of a user-supplied directory. Because configuring front end build tools kinda sucks.
+# Reloads the "browser" when it detects changes to the byte size of a user-supplied directory.
+# Because configuring front-end build tools kinda sucks.
 
 import os
 import time
