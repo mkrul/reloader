fdaa0c731089c09a3b0d7ed9962f7f96c4cde73f
mkrul <mishakrul@gmail.com>
2019-03-04 08:17:34 -0500

    add linux compatibility

--- reloader.py
+++ reloader.py
@@ -3,25 +3,30 @@
 import os
 import time
 from selenium import webdriver
+from checksumdir import dirhash
 
 def main():
     dir_path = get_file_path()
     port = input("Port number to monitor: ")
     url = "http://localhost:{}/".format(port)
-    driver = get_browser()
+    browser = get_browser()
+    driver = get_os(browser)
     driver.get(url)
     while True:
-        size1 = get_size(dir_path)
+        hash1 = dirhash(dir_path, "md5")
+        print(hash1)
         time.sleep(2)
-        size2 = get_size(dir_path)
-        sizes_are_different = compare(size1, size2)
-        if sizes_are_different:
+        hash2 = dirhash(dir_path, "md5")
+        print(hash2)
+        hashes_are_different = compare(hash1, hash2)
+        if hashes_are_different:
             driver.refresh()
 
 def get_file_path():
     path_exists = False
     while path_exists == False:
         dir_path = input("Enter the absolute path of the directory: ")
+        print(dir_path)
         if os.path.exists(dir_path):
             return dir_path
         else:
@@ -32,22 +37,35 @@ def get_browser():
     while selection != "1" or "2":
         browser = input("Type 1 to use Chrome, or 2 to use Firefox: ")
         if browser == "1":
-            return webdriver.Chrome(executable_path="./chromedriver.exe")
+            return "1"
         elif browser == "2":
-            return webdriver.Firefox(executable_path="./geckodriver.exe")
+            return "2"
         else:
             print("Invalid choice!")
 
-def get_size(dir_path):
-    dir_size = 0
-    for (path, dirs, files) in os.walk(dir_path):
-        for file in files:
-            filename = os.path.join(path, file)
-            dir_size += os.path.getsize(filename)
-    return dir_size
+def get_os(browser):
+    selection = ""
+    if browser == "1":
+        while selection != "1" or "2":
+            operating_system = input("Type 1 if you are using Windows, or 2 for Linux: ")
+            if operating_system == "1":
+                return webdriver.Chrome(executable_path="./chromedriver.exe")
+            elif operating_system == "2":
+                return webdriver.Chrome(executable_path="./chromedriver-linux")
+            else:
+                print("Invalid choice!")
+    if browser == "2":
+        while selection != "1" or "2":
+            operating_system = input("Type 1 if you are using Windows, or 2 for Linux: ")
+            if operating_system == "1":
+                return webdriver.Firefox(executable_path="./geckodriver.exe")
+            elif operating_system == "2":
+                return webdriver.Firefox(executable_path="./geckodriver-linux")
+            else:
+                print("Invalid choice!")
 
-def compare(size1, size2):
-    if size1 != size2:
+def compare(hash1, hash2):
+    if hash1 != hash2:
         return True
     else:
         return False
